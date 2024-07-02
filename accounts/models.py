cbed75a9f11f4dd8a1e3d8e13715218d9982e644
13virat <viratgupta1314@gmail.com>
2024-07-02 13:33:02 +0530

    updated transaction

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Account(models.Model):
     ACCOUNT_TYPES = [
         ('SAVINGS', 'Savings'),
@@ -15,20 +16,6 @@ class Account(models.Model):
     def __str__(self):
         return f'{self.user.username} - {self.account_type}'
 
-class Transaction(models.Model):
-    TRANSACTION_TYPES = [
-        ('DEPOSIT', 'Deposit'),
-        ('WITHDRAWAL', 'Withdrawal'),
-        ('TRANSFER', 'Transfer'),
-    ]
-
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    transaction_type = models.CharField(max_length=20, choices=TRANSACTION_TYPES)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f'{self.transaction_type} - {self.amount}'
     
 class BankAccount(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
