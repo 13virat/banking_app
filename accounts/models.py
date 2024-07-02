06d9174153be4b21033f98feda3f6d8e6ab138ae
13virat <viratgupta1314@gmail.com>
2024-07-02 09:01:03 +0530

    Initial commit with project setup and models

---
+++ accounts/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Account(models.Model):
+    ACCOUNT_TYPES = [
+        ('SAVINGS', 'Savings'),
+        ('CHECKING', 'Checking'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_type = models.CharField(max_length=20, choices=ACCOUNT_TYPES)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.account_type}'
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('DEPOSIT', 'Deposit'),
+        ('WITHDRAWAL', 'Withdrawal'),
+        ('TRANSFER', 'Transfer'),
+    ]
+
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=20, choices=TRANSACTION_TYPES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.transaction_type} - {self.amount}'
